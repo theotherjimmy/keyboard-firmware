97fa6b4c43091eef7023f2cbee2023982732d491
Jimmy Brisson <theotherjimmy@gmail.com>
2014-01-04 15:00:10 -0600

    expanded init_matrix definition to take pins for each of rows and columns made init_matrix only allocate new matricies when there are keys to scan.

--- src/matrix-driver.c
+++ src/matrix-driver.c
@@ -6,10 +6,15 @@ keymatrix_t matricies[num_matricies];
 uint32_t allocated = 0;
 
 keybits_t scan_matrix(keymatrix_t *to_scan){
+  if (to_scan == (keymatrix_t *) NULL) return (keybits_t) 0;
   return (keybits_t) 0;
 }
 
-keymatrix_t* init_matrix(){
-  if (allocated >= num_matricies) return (keymatrix_t *) NULL;
+keymatrix_t* init_matrix(Pin_t *columns, uint32_t num_columns,
+			 Pin_t *rows,    uint32_t num_rows){
+  if ((allocated >= num_matricies) ||
+      (num_columns == 0 ) ||
+      (num_rows == 0))
+    return (keymatrix_t *) NULL;
   else return &(matricies[allocated++]);
 }
