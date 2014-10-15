2983113e407f3fc0672095436cc67cb48cc191f5
Jimmy Brisson <theotherjimmy@gmail.com>
2014-10-15 01:24:46 -0500

    hid-keyboard.[ch] match

--- src/hid-keyboard.h
+++ src/hid-keyboard.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-extern bool USBDHIDKeyboardInit(uint32_t index, keymatrix_t * to_scan_left, keymatrix_t * to_scan_right,
-				keymatrix_t * to_scan_thumb_left, keymatrix_t * to_scan_thumb_right);
-
+extern bool USBDHIDKeyboardInit(uint32_t index);
+extern bool SendButtons ( ) ;
 #endif //__HID_KEYBOARD__
